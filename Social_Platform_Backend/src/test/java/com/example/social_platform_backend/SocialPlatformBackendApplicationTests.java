482b8d04a25d3d12a23844cd837de919b45bde58
IoachimSotan <s.ioachim@yahoo.com>
2024-04-09 21:27:52 +0300

    Initial Commit

---
+++ Social_Platform_Backend/src/test/java/com/example/social_platform_backend/SocialPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.social_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
