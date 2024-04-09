81fa74abd15df78dc94c64a360d88fbffee4df84
IoachimSotan <s.ioachim@yahoo.com>
2024-04-09 21:12:08 +0300

    Initial commit

---
+++ Social_Platform_Frontend/src/main/java/com/example/social_platform_frontend/SocialPlatformFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.social_platform_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPlatformFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialPlatformFrontendApplication.class, args);
+	}
+
+}
---
+++ Social_Platform_Frontend/src/test/java/com/example/social_platform_frontend/SocialPlatformFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.social_platform_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPlatformFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
