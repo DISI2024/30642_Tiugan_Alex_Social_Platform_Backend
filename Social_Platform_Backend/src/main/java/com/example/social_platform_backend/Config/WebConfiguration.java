74f5aef36d1e71870d737fa242adbf62b2abee87
vladradu <mevladradu@gmail.com>
2024-04-23 18:35:15 +0300

    configured cors policy

---
+++ Social_Platform_Backend/src/main/java/com/example/social_platform_backend/Config/WebConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.social_platform_backend.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@CrossOrigin("*")
+public class WebConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
