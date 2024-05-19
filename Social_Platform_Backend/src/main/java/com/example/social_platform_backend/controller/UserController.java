64b503190dd9197c4c87036efdd887a3a92b98bf
mihaligabriel <mihaligabriel75@gmail.com>
2024-05-19 19:55:39 +0300

    Removed imports.

--- Social_Platform_Backend/src/main/java/com/example/social_platform_backend/controller/UserController.java
+++ Social_Platform_Backend/src/main/java/com/example/social_platform_backend/controller/UserController.java
@@ -2,13 +2,11 @@ package com.example.social_platform_backend.controller;
 
 import com.example.social_platform_backend.facade.ResetPasswordDTO;
 import com.example.social_platform_backend.facade.User;
-import com.example.social_platform_backend.facade.UsernameDTO;
 import com.example.social_platform_backend.service.EmailService;
 import com.example.social_platform_backend.service.ResetPasswordService;
 import com.example.social_platform_backend.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- Social_Platform_Backend/src/main/java/com/example/social_platform_backend/service/EmailService.java
+++ Social_Platform_Backend/src/main/java/com/example/social_platform_backend/service/EmailService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 import java.util.Random;
-import java.util.UUID;
 
 @Service
 public class EmailService {
